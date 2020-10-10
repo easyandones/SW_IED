292908477c3473bd83b47f6ce50c480aff3a1912
easyandones <66263556+easyandones@users.noreply.github.com>
2020-10-10 22:36:30 +0900

    09C1

---
+++ 09C1.ino
@@ -0,0 +1,109 @@
+// Arduino pin assignment
+#define PIN_LED 9
+#define PIN_TRIG 12
+#define PIN_ECHO 13
+
+// configurable parameters
+#define SND_VEL 346.0 // sound velocity at 24 celsius degree (unit: m/s)
+#define INTERVAL 25 // sampling interval (unit: ms)
+#define _DIST_MIN 100 // minimum distance to be measured (unit: mm)
+#define _DIST_MAX 300 // maximum distance to be measured (unit: mm)
+#define RES_LEN 30
+
+// global variables
+float timeout; // unit: us
+float dist_min, dist_max, dist_raw, median; // unit: mm
+unsigned long last_sampling_time; // unit: ms
+float scale; // used for pulse duration to distance conversion
+float results[RES_LEN] = {0,};
+
+void save_data(int data) {
+  for (int i = 0; i < RES_LEN - 1; i++) {
+    results[i] = results[i + 1];
+  }
+  results[RES_LEN - 1] = data;
+}
+
+float get_middle(float sort_array[]) {
+  float new_array[RES_LEN];
+  for (int i = 0; i < RES_LEN; i++) {
+    new_array[i] = sort_array[i];
+  }
+  int temp;
+  for (int i = 0; i < RES_LEN; i++) {
+    for (int j = 0; j < RES_LEN - 1; j++) {
+      if (new_array[j] > new_array[j + 1]) {
+        temp = new_array[j];
+        new_array[j] = new_array[j + 1];
+        new_array[j + 1] = temp;
+      }
+    }
+  }
+  return new_array[RES_LEN / 2];
+}
+
+void setup() {
+  // initialize GPIO pins
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);
+  digitalWrite(PIN_TRIG, LOW);
+  pinMode(PIN_ECHO, INPUT);
+
+  // initialize USS related variables
+  dist_min = _DIST_MIN;
+  dist_max = _DIST_MAX;
+  timeout = (INTERVAL / 2) * 1000.0; // precalculate pulseIn() timeout value. (unit: us)
+  dist_raw = 0.0; // raw distance output from USS (unit: mm)
+  scale = 0.001 * 0.5 * SND_VEL;
+
+  // initialize serial port
+  Serial.begin(57600);
+
+  // initialize last sampling time
+  last_sampling_time = 0;
+}
+
+void loop() {
+  // wait until next sampling time.
+  // millis() returns the number of milliseconds since the program started. Will overflow after 50 days.
+  if (millis() < last_sampling_time + INTERVAL) return;
+
+  // get a distance reading from the USS
+  dist_raw = USS_measure(PIN_TRIG, PIN_ECHO);
+  save_data(dist_raw);
+  median = get_middle(results);
+
+  // output the read value to the serial port
+  Serial.print("Min:0,");
+  Serial.print("raw:");
+  Serial.print(dist_raw);
+  Serial.print(",");
+  Serial.print("median:");
+  //Serial.print(median);
+  Serial.print(map(median, 0, 400, 100, 500));
+  Serial.print(",");
+  Serial.println("Max:500");
+
+  // turn on the LED if the distance is between dist_min and dist_max
+  if (dist_raw < dist_min || dist_raw > dist_max) {
+    analogWrite(PIN_LED, 255);
+  }
+  else {
+    analogWrite(PIN_LED, 0);
+  }
+
+  // update last sampling time
+  last_sampling_time += INTERVAL;
+}
+
+// get a distance reading from USS. return value is in millimeter.
+float USS_measure(int TRIG, int ECHO)
+{
+  float reading;
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  reading = pulseIn(ECHO, HIGH, timeout) * scale; // unit: mm
+  if (reading < dist_min || reading > dist_max) reading = 0.0; // return 0 when out of range.
+  return reading;
+}
